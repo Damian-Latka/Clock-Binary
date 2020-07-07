29896ef79e4dcf11397c77359ea53476605c48e0
Damian-Latka <damian.latka99@gmail.com>
2020-07-07 21:14:33 +0200

    small changes + github link

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 let time, hours, minutes, seconds;
 let digital_clock = document.getElementById("digital-clock");
+let turned_off = "rgba(53,36,121,1)";
+let turned_on = "rgba(231,227,247,1)";
 getTime();
-setInterval(getTime, 1000);
+//interval is responsible for refreshing the time every 200ms
+setInterval(getTime, 200);
 
 //function that gets current system time
 function getTime(){
@@ -28,45 +31,45 @@ function xd(){
     let max;
     //show hours on binary clock
     for(i = 0; i < 2; i++){
-        //convert each digit to binary
+        //checking each digit separately
         max = document.getElementById(`col${i}`).children.length;
         num = hours.toString(10).charAt(i);
         for(j = 0; j < max; j++){
             //if the first bit is a 1, light up the "LED"
             if((num & 1) === 1){
-                document.getElementById(`col${i}`).children.item(j).style.background = "rgb(218,125,101)";
+                document.getElementById(`col${i}`).children.item(j).style.background = turned_on;
             }else{
-                document.getElementById(`col${i}`).children.item(j).style.background = "rgb(221, 200, 200)";
+                document.getElementById(`col${i}`).children.item(j).style.background = turned_off;
             }
             num = num >> 1;
         }
     }
     //show minutes on binary clock
     for(i = 2; i < 4; i++){
-        //convert each digit to binary
+        //checking each digit separately
         max = document.getElementById(`col${i}`).children.length;
         num = minutes.toString(10).charAt(i-2);
         for(j = 0; j < max; j++){
             //if the first bit is a 1, light up the "LED"
             if((num & 1) === 1){
-                document.getElementById(`col${i}`).children.item(j).style.background = "rgb(218,125,101)";
+                document.getElementById(`col${i}`).children.item(j).style.background = turned_on;
             }else{
-                document.getElementById(`col${i}`).children.item(j).style.background = "rgb(221, 200, 200)";
+                document.getElementById(`col${i}`).children.item(j).style.background = turned_off;
             }
             num = num >> 1;
         }
     }
     //show seconds on binary clock
     for(i = 4; i < 6; i++){
-        //convert each digit to binary
+        //checking each digit separately
         max = document.getElementById(`col${i}`).children.length;
         num = seconds.toString(10).charAt(i-4);
         for(j = 0; j < max; j++){
             //if the first bit is a 1, light up the "LED"
             if((num & 1) === 1){
-                document.getElementById(`col${i}`).children.item(j).style.background = "rgb(218,125,101)";
+                document.getElementById(`col${i}`).children.item(j).style.background = turned_on;
             }else{
-                document.getElementById(`col${i}`).children.item(j).style.background = "rgb(221, 200, 200)";
+                document.getElementById(`col${i}`).children.item(j).style.background = turned_off;
             }
             num = num >> 1;
         }
